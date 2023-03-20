a09d5a7538dccc0367f925c576e7e61c4a4972b7
ChimichangasBoi <91523145+ChimichangasBoi@users.noreply.github.com>
2023-03-20 06:36:48 +0530

    Update test.py

--- test.py
+++ test.py
@@ -9,6 +9,7 @@ from sklearn.neighbors import NearestNeighbors
 import cv2
 
 
+
 #(total images,2048)
 feature_list = np.array(pickle.load(open('embeddings.pkl','rb')))
 filenames = pickle.load(open('filenames.pkl','rb'))
